e7e5e1cde98ac0850d558b767afa78d5a16e96a4
EPILIASHOKKUMAR <eppiliashokkumara@gmail.com>
2025-12-15 18:07:32 +0530

    Add Auth module (login/signup) and Admin module

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'admin/dashboard', component: AdminDashboardComponent }
+];
