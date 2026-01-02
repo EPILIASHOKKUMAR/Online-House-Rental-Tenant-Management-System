491a4dd1efdaafa31f72f077d8c685f20b91fc69
EPILIASHOKKUMAR <eppiliashokkumara@gmail.com>
2026-01-02 16:50:19 +0530

    deploy changes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://YOUR-BACKEND-URL.onrender.com/api',
-  socketUrl: 'https://YOUR-BACKEND-URL.onrender.com'
+  apiUrl: 'https://online-house-rental-tenant-management.onrender.com/api',
+  socketUrl: 'https://online-house-rental-tenant-management.onrender.com'
 };
